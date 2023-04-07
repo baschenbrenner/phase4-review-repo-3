ac2c4b446f3ce866f6b38039d42cf8d103ea6738
andrew <andrewasmit@hotmail.com>
2023-04-07 10:33:12 -0500

    updated seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,20 @@ User.create(
     password: "ABC123", 
     email: Faker::Internet.email )
 
-10.times do 
+User.create(
+    username: "test", 
+    password: "123", 
+    email: Faker::Internet.email )
 
+10.times do 
 Client.create(
     name:Faker::Company.name, 
     point_of_contact: Faker::Name.name, 
     poc_email: Faker::Internet.email )
+end
 
-Invoice.create(
+20.times do 
+Invoice.create( 
     cost: Faker::Invoice.amount_between(from: 100, to: 5000), 
     date_invoice_sent:Faker::Date.backward(days: 365), 
     date_invoice_paid: Faker::Date.forward(days: 365), 
@@ -23,8 +29,7 @@ Invoice.create(
     date_service_completed:Faker::Date.forward(days: 365), 
     service_description:Faker::Lorem.paragraph, 
     client_id: Faker::Number.between(from: 1, to: 10),
-    user_id: 1)
-
+    user_id: Faker::Number.between(from: 1, to: 2) )
 end
 
 
